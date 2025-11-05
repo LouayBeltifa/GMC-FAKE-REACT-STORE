ab24c78eb688e7dd1b50c320352e692f85d08a54
mac <chiamakaesther098@gmail.com>
2025-11-05 21:02:06 +0100

    update on hero

--- src/components/HomeComponents/Hero.jsx
+++ src/components/HomeComponents/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className="w-1/2 flex items-center justify-center flex-col">
         <h2 className="text-2xl font-bold">Welcome to our fake store</h2>
         <p className="text-gray-400 mb-5">
-          Many products, luxury items, low price.
+          Many products, luxury items, low price!.
         </p>
         <ButtonComponent title="Start Shopping" />
       </div>
