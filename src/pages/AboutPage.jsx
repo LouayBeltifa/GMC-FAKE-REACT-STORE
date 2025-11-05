07e3df2836b030a3571aca9404bc1da70c68787c
Louay Beltifa <louay.beltifa1@gmail.com>
2025-11-05 20:33:45 +0100

    LOUAY BELTIFA : Task for about us page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,15 @@
 function AboutPage() {
-  return <div>TODO : AboutPage</div>;
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aut
+        voluptatum dignissimos. Tempora, alias nihil! Culpa tempora odio
+        corporis accusamus asperiores in, fugit sed, tenetur, laboriosam optio
+        eligendi veniam repellendus.
+      </p>
+    </div>
+  );
 }
 
 export default AboutPage;
